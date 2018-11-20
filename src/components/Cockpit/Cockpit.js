0ec828b88edaf0a21e678c92c37877cca195650d
Henrique de O. Fadoni <hofadoni@users.noreply.github.com>
2018-11-20 16:54:20 -0200

    Returning Adjacent Elements( React 16+ )

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 const cockpit = props => {
     const assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
 
     if (props.showPersons) {
-        btnClass = classes.Red;
+        btnClass = [classes.Button, classes.red].join(' ');
     }
     if (props.persons.length <= 2) {
         assignedClasses.push(classes.red); //classes = ['red']
@@ -16,13 +17,13 @@ const cockpit = props => {
     }
 
     return (
-        <div className={classes.Cockpit}>
+        <Aux>
           <h1>Hi, I'm a React App</h1>
           <p className={assignedClasses.join(' ')}>This is really working!</p>
           <button
             className={btnClass}
             onClick={props.clicked}>Switch Name</button>
-        </div>
+        </Aux>
     );
 };
 
