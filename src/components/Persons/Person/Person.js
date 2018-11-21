927fbc26423feb9d3b785ceca6f01a101e702679
Henrique de O. Fadoni <hofadoni@users.noreply.github.com>
2018-11-21 18:10:17 -0200

    Using References ("ref")

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,12 +3,22 @@ import classes from './Person.css';
 import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
+    componentDidMount () {
+        if (this.props.position === 0){
+            this.inputElement.focus();
+        }   
+    }
+
     render() {
         return (
             <WithClass classes={classes.Person}>
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                    ref = {input => this.inputElement = input}
+                    type="text"
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
             </WithClass>
         )
     }
