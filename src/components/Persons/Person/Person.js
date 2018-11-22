1c4de767b381c0376404147574331ae5ad9ee4f4
Henrique de O. Fadoni <hofadoni@users.noreply.github.com>
2018-11-22 11:33:15 -0200

    The context API ( React 16.3 )

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,10 +3,19 @@ import classes from './Person.css';
 import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
+    constructor (props) {
+        super(props);
+        this.inputElement = React.createRef();
+    }
+
     componentDidMount () {
         if (this.props.position === 0){
-            this.inputElement.focus();
+            this.inputElement.current.focus();
         }   
+    }
+
+    focus () {
+        this.inputElement.current.focus();
     }
 
     render() {
@@ -15,7 +24,7 @@ class Person extends Component {
                 <p onClick={this.props.click}> I'm {this.props.name} and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref = {input => this.inputElement = input}
+                    ref = {this.inputElement}
                     type="text"
                     onChange={this.props.changed} 
                     value={this.props.name} />
